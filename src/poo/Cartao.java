96de922ce048c0afc26aa0fbe9ccf57bf04c3df3
miguelsantos1719 <79711981+miguelsantos1719@users.noreply.github.com>
2021-08-18 12:34:16 +0100

    Add files via upload

---
+++ src/poo/Cartao.java
@@ -0,0 +1,62 @@
+package poo;
+
+/**
+ * A classe Cartao contém métodos para realizar operações 
+ * @author miguelsantos
+ *
+ */
+public class Cartao {
+	
+	private String nomeFuncionario;
+	private String servico;
+	private String cargo;
+	
+	// Construtores 
+	public Cartao(String nomeFuncionario) {
+		super();
+		this.nomeFuncionario = nomeFuncionario;
+	}
+
+	public Cartao(String nomeFuncionario, String servico, String cargo) {
+		super();
+		this.nomeFuncionario = nomeFuncionario;
+		this.servico = servico;
+		this.cargo = cargo;
+	}
+
+	// Acessores
+	
+	public String getNomeFuncionario() {
+		return nomeFuncionario;
+	}
+
+	public void setServico(String servico) {
+		this.servico = servico;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	
+	// Métodos
+	public void retirarServico() {
+		// servico = "";
+		setServico("");
+	}
+	
+	public void atribuirNovoServico( String novoServico ) {
+		//servico = novoServico;
+		setServico( novoServico );
+	}
+	
+	public void promoverNovoCargo( String novoCargo ) {
+		//cargo = novoCargo;
+		setCargo(novoCargo);
+	}
+	
+	public void despedirFuncionario() {
+		retirarServico();
+		setCargo("");
+	}
+	
+}
